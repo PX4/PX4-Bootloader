3b1760ddd40058a942ed0a4805e1fb5083f298ad
Lorenz Meier <lm@inf.ethz.ch>
2015-01-11 21:14:27 +0100

    Main F4 code: Fix style

--- main_f4.c
+++ main_f4.c
@@ -19,30 +19,30 @@ static struct {
 	uint32_t	size;
 } flash_sectors[] = {
 	/* flash sector zero reserved for bootloader */
-		{0x01, 16 * 1024},
-		{0x02, 16 * 1024},
-		{0x03, 16 * 1024},
-		{0x04, 64 * 1024},
-		{0x05, 128 * 1024},
-		{0x06, 128 * 1024},
-		{0x07, 128 * 1024},
-		{0x08, 128 * 1024},
-		{0x09, 128 * 1024},
-		{0x0a, 128 * 1024},
-		{0x0b, 128 * 1024},
-		/* flash sectors only in 2MiB devices */
-		{0x10, 16 * 1024},
-		{0x11, 16 * 1024},
-		{0x12, 16 * 1024},
-		{0x13, 16 * 1024},
-		{0x14, 64 * 1024},
-		{0x15, 128 * 1024},
-		{0x16, 128 * 1024},
-		{0x17, 128 * 1024},
-		{0x18, 128 * 1024},
-		{0x19, 128 * 1024},
-		{0x1a, 128 * 1024},
-		{0x1b, 128 * 1024},
+	{0x01, 16 * 1024},
+	{0x02, 16 * 1024},
+	{0x03, 16 * 1024},
+	{0x04, 64 * 1024},
+	{0x05, 128 * 1024},
+	{0x06, 128 * 1024},
+	{0x07, 128 * 1024},
+	{0x08, 128 * 1024},
+	{0x09, 128 * 1024},
+	{0x0a, 128 * 1024},
+	{0x0b, 128 * 1024},
+	/* flash sectors only in 2MiB devices */
+	{0x10, 16 * 1024},
+	{0x11, 16 * 1024},
+	{0x12, 16 * 1024},
+	{0x13, 16 * 1024},
+	{0x14, 64 * 1024},
+	{0x15, 128 * 1024},
+	{0x16, 128 * 1024},
+	{0x17, 128 * 1024},
+	{0x18, 128 * 1024},
+	{0x19, 128 * 1024},
+	{0x1a, 128 * 1024},
+	{0x1b, 128 * 1024},
 };
 #define BOOTLOADER_RESERVATION_SIZE	(16 * 1024)
 
@@ -170,8 +170,7 @@ static void board_init(void);
 #define BOOT_RTC_REG		MMIO32(RTC_BASE + 0x50)
 
 /* standard clocking for all F4 boards */
-static const clock_scale_t clock_setup =
-{
+static const clock_scale_t clock_setup = {
 	.pllm = OSC_FREQ,
 	.plln = 336,
 	.pllp = 2,
@@ -231,24 +230,34 @@ board_test_force_pin()
 
 	for (volatile unsigned cycles = 0; cycles < 10; cycles++) {
 		gpio_set(BOARD_FORCE_BL_PORT, BOARD_FORCE_BL_PIN_OUT);
+
 		for (unsigned count = 0; count < 20; count++) {
-			if (gpio_get(BOARD_FORCE_BL_PORT, BOARD_FORCE_BL_PIN_IN) != 0)
+			if (gpio_get(BOARD_FORCE_BL_PORT, BOARD_FORCE_BL_PIN_IN) != 0) {
 				vote++;
+			}
+
 			samples++;
 		}
+
 		gpio_clear(BOARD_FORCE_BL_PORT, BOARD_FORCE_BL_PIN_OUT);
+
 		for (unsigned count = 0; count < 20; count++) {
-			if (gpio_get(BOARD_FORCE_BL_PORT, BOARD_FORCE_BL_PIN_IN) == 0)
+			if (gpio_get(BOARD_FORCE_BL_PORT, BOARD_FORCE_BL_PIN_IN) == 0) {
 				vote++;
+			}
+
 			samples++;
 		}
 	}
+
 	/* revert the driver pin */
 	gpio_mode_setup(BOARD_FORCE_BL_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, BOARD_FORCE_BL_PIN_OUT);
 
 	/* the idea here is to reject wire-to-wire coupling, so require > 90% agreement */
-	if ((vote * 100) > (samples * 90))
+	if ((vote * 100) > (samples * 90)) {
 		return true;
+	}
+
 #endif
 #if defined(BOARD_FORCE_BL_PIN)
 	/* single pin pulled up or down */
@@ -260,13 +269,16 @@ board_test_force_pin()
 	gpio_mode_setup(BOARD_FORCE_BL_PORT, GPIO_MODE_INPUT, BOARD_FORCE_BL_PULL, BOARD_FORCE_BL_PIN);
 
 	for (samples = 0; samples < 200; samples++) {
-		if ((gpio_get(BOARD_FORCE_BL_PORT, BOARD_FORCE_BL_PIN) ? 1 : 0) == BOARD_FORCE_BL_STATE)
+		if ((gpio_get(BOARD_FORCE_BL_PORT, BOARD_FORCE_BL_PIN) ? 1 : 0) == BOARD_FORCE_BL_STATE) {
 			vote++;
+		}
 	}
 
 	/* reject a little noise */
-	if ((vote * 100) > (samples * 90))
+	if ((vote * 100) > (samples * 90)) {
 		return true;
+	}
+
 #endif
 	return false;
 }
@@ -278,16 +290,16 @@ board_init(void)
 	board_info.fw_size = APP_SIZE_MAX,
 
 #ifdef INTERFACE_USB
-	/* enable GPIO9 with a pulldown to sniff VBUS */
-	rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPAEN);
+		   /* enable GPIO9 with a pulldown to sniff VBUS */
+		   rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_AHB1ENR_IOPAEN);
 	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_PULLDOWN, GPIO9);
 #endif
 
 	/* initialise LEDs */
 	rcc_peripheral_enable_clock(&RCC_AHB1ENR, BOARD_CLOCK_LEDS);
 	gpio_mode_setup(
-		BOARD_PORT_LEDS, 
-		GPIO_MODE_OUTPUT, 
+		BOARD_PORT_LEDS,
+		GPIO_MODE_OUTPUT,
 		GPIO_PUPD_NONE,
 		BOARD_PIN_LED_BOOTLOADER | BOARD_PIN_LED_ACTIVITY);
 	gpio_set_output_options(
@@ -295,7 +307,7 @@ board_init(void)
 		GPIO_OTYPE_PP,
 		GPIO_OSPEED_2MHZ,
 		BOARD_PIN_LED_BOOTLOADER | BOARD_PIN_LED_ACTIVITY);
-	BOARD_LED_ON (
+	BOARD_LED_ON(
 		BOARD_PORT_LEDS,
 		BOARD_PIN_LED_BOOTLOADER | BOARD_PIN_LED_ACTIVITY);
 
@@ -309,25 +321,31 @@ board_init(void)
 uint32_t
 flash_func_sector_size(unsigned sector)
 {
-	if (sector < BOARD_FLASH_SECTORS)
+	if (sector < BOARD_FLASH_SECTORS) {
 		return flash_sectors[sector].size;
+	}
+
 	return 0;
 }
 
 void
 flash_func_erase_sector(unsigned sector)
 {
-	if (sector >= BOARD_FLASH_SECTORS)
+	if (sector >= BOARD_FLASH_SECTORS) {
 		return;
+	}
 
 	/* get the base address of the sector */
 	uint32_t address = 0;
-	for (unsigned i = 0; i < sector; i++)
+
+	for (unsigned i = 0; i < sector; i++) {
 		address += flash_func_sector_size(i);
+	}
 
 	/* blank-check the sector */
 	unsigned size = flash_func_sector_size(sector);
 	bool blank = true;
+
 	for (unsigned i = 0; i < size; i += sizeof(uint32_t)) {
 		if (flash_func_read_word(address + i) != 0xffffffff) {
 			blank = false;
@@ -336,8 +354,9 @@ flash_func_erase_sector(unsigned sector)
 	}
 
 	/* erase the sector if it failed the blank check */
-	if (!blank)
+	if (!blank) {
 		flash_erase_sector(flash_sectors[sector].sector_number, FLASH_CR_PROGRAM_X32);
+	}
 }
 
 void
@@ -346,21 +365,26 @@ flash_func_write_word(uint32_t address, uint32_t word)
 	flash_program_word(address + APP_LOAD_ADDRESS, word);
 }
 
-uint32_t 
+uint32_t
 flash_func_read_word(uint32_t address)
 {
-	if (address & 3)
+	if (address & 3) {
 		return 0;
+	}
+
 	return *(uint32_t *)(address + APP_LOAD_ADDRESS);
 }
 
 uint32_t
 flash_func_read_otp(uint32_t address)
 {
-	if (address & 3)
+	if (address & 3) {
 		return 0;
-	if (address > OTP_SIZE)
+	}
+
+	if (address > OTP_SIZE) {
 		return 0;
+	}
 
 	return *(uint32_t *)(address + OTP_BASE);
 }
@@ -368,18 +392,19 @@ uint32_t
 flash_func_read_sn(uint32_t address)
 {
 	// read a byte out from unique chip ID area
-	// it's 12 bytes, or 3 words. 
-    return *(uint32_t *)(address + UDID_START);
+	// it's 12 bytes, or 3 words.
+	return *(uint32_t *)(address + UDID_START);
 }
 void
 led_on(unsigned led)
 {
 	switch (led) {
 	case LED_ACTIVITY:
-		BOARD_LED_ON (BOARD_PORT_LEDS, BOARD_PIN_LED_ACTIVITY);
+		BOARD_LED_ON(BOARD_PORT_LEDS, BOARD_PIN_LED_ACTIVITY);
 		break;
+
 	case LED_BOOTLOADER:
-		BOARD_LED_ON (BOARD_PORT_LEDS, BOARD_PIN_LED_BOOTLOADER);
+		BOARD_LED_ON(BOARD_PORT_LEDS, BOARD_PIN_LED_BOOTLOADER);
 		break;
 	}
 }
@@ -389,10 +414,11 @@ led_off(unsigned led)
 {
 	switch (led) {
 	case LED_ACTIVITY:
-		BOARD_LED_OFF (BOARD_PORT_LEDS, BOARD_PIN_LED_ACTIVITY);
+		BOARD_LED_OFF(BOARD_PORT_LEDS, BOARD_PIN_LED_ACTIVITY);
 		break;
+
 	case LED_BOOTLOADER:
-		BOARD_LED_OFF (BOARD_PORT_LEDS, BOARD_PIN_LED_BOOTLOADER);
+		BOARD_LED_OFF(BOARD_PORT_LEDS, BOARD_PIN_LED_BOOTLOADER);
 		break;
 	}
 }
@@ -404,6 +430,7 @@ led_toggle(unsigned led)
 	case LED_ACTIVITY:
 		gpio_toggle(BOARD_PORT_LEDS, BOARD_PIN_LED_ACTIVITY);
 		break;
+
 	case LED_BOOTLOADER:
 		gpio_toggle(BOARD_PORT_LEDS, BOARD_PIN_LED_BOOTLOADER);
 		break;
@@ -422,12 +449,12 @@ main(void)
 	unsigned timeout = BOOTLOADER_DELAY;	/* if nonzero, drop out of the bootloader after this time */
 
 	/* Enable the FPU before we hit any FP instructions */
-	SCB_CPACR |= ((3UL << 10*2) | (3UL << 11*2)); /* set CP10 Full Access and set CP11 Full Access */
+	SCB_CPACR |= ((3UL << 10 * 2) | (3UL << 11 * 2)); /* set CP10 Full Access and set CP11 Full Access */
 
 	/* do board-specific initialisation */
 	board_init();
 
-	/* 
+	/*
 	 * Check the force-bootloader register; if we find the signature there, don't
 	 * try booting.
 	 */
@@ -443,7 +470,7 @@ main(void)
 		 */
 		timeout = 0;
 
-		/* 
+		/*
 		 * Clear the signature so that if someone resets us while we're
 		 * in the bootloader we'll try to boot next time.
 		 */
@@ -451,6 +478,7 @@ main(void)
 	}
 
 #ifdef INTERFACE_USB
+
 	/*
 	 * Check for USB connection - if present, don't try to boot, but set a timeout after
 	 * which we will fall out of the bootloader.
@@ -463,6 +491,7 @@ main(void)
 		/* don't try booting before we set up the bootloader */
 		try_boot = false;
 	}
+
 #endif
 
 	/* Try to boot the app if we think we should just go straight there */
@@ -495,14 +524,14 @@ main(void)
 	gpio_set_af(GPIOC, GPIO_AF0, GPIO9);
 #endif
 
-	while (1)
-	{
+	while (1) {
 		/* run the bootloader, come back after an app is uploaded or we time out */
 		bootloader(timeout);
 
 		/* if the force-bootloader pins are strapped, just loop back */
-		if (board_test_force_pin())
+		if (board_test_force_pin()) {
 			continue;
+		}
 
 		/* set the boot-to-bootloader flag so that if boot fails on reset we will stop here */
 #ifdef BOARD_BOOT_FAIL_DETECT
