f33af35eee022e1c0d246c2a35eff66f84a4d120
Anthony Kenga <anton.k@airdog.com>
2017-08-01 21:11:16 +0300

    Changed hw configuration for F0 chip to match our schematic

--- hw_config.h
+++ hw_config.h
@@ -741,50 +741,51 @@
 # define BOARD_FORCE_BL_STATE			1
 
 /****************************************************************************
- * TARGET_HW_PX4_PIO_V1 or TARGET_HW_PX4_PIO_V2
+ * TARGET_HW_AD_GOPRO_CHIP
  ****************************************************************************/
 
-#elif  defined(TARGET_HW_PX4_PIO_V1) ||  defined(TARGET_HW_PX4_PIO_V2)
+#elif  defined(TARGET_HW_AD_GOPRO_CHIP)
 
 # define APP_LOAD_ADDRESS               0x08001000
 # define APP_SIZE_MAX                   0xf000
-# define BOOTLOADER_DELAY               200
-# define BOARD_PIO
-# define INTERFACE_USB                	0
+# define BOOTLOADER_DELAY               1000 // ??!!! Will it cause problems for I2C communication? Can we allow the main firmware to get booted much later?
+# define INTERFACE_USB                  0
 # define INTERFACE_USART                1
 # define USBDEVICESTRING                ""
 # define USBPRODUCTID                   -1
 
 # define OSC_FREQ                       24
 
-# define BOARD_PIN_LED_ACTIVITY         GPIO14
-# define BOARD_PIN_LED_BOOTLOADER       GPIO15
-# define BOARD_PORT_LEDS                GPIOB
-# define BOARD_CLOCK_LEDS_REGISTER      RCC_APB2ENR
-# define BOARD_CLOCK_LEDS               RCC_APB2ENR_IOPBEN
-# define BOARD_LED_ON                   gpio_clear
-# define BOARD_LED_OFF                  gpio_set
+// LEDs are not present on our board
+//# define BOARD_PIN_LED_ACTIVITY         GPIO14
+//# define BOARD_PIN_LED_BOOTLOADER       GPIO15
+//# define BOARD_PORT_LEDS                GPIOB
+//# define BOARD_CLOCK_LEDS_REGISTER      RCC_APB2ENR
+//# define BOARD_CLOCK_LEDS               RCC_APB2ENR_IOPBEN
+//# define BOARD_LED_ON                   gpio_clear
+//# define BOARD_LED_OFF                  gpio_set
 
-# define BOARD_USART                    USART2
-# define BOARD_USART_CLOCK_REGISTER     RCC_APB1ENR
-# define BOARD_USART_CLOCK_BIT          RCC_APB1ENR_USART2EN
+# define BOARD_USART                    USART1
+# define BOARD_USART_CLOCK_REGISTER     RCC_APB2ENR
+# define BOARD_USART_CLOCK_BIT          RCC_APB2ENR_USART1EN
 
 # define BOARD_PORT_USART               GPIOA
-# define BOARD_PIN_TX                   GPIO_USART2_TX
-# define BOARD_PIN_RX                   GPIO_USART2_RX
-# define BOARD_USART_PIN_CLOCK_REGISTER RCC_APB2ENR
-# define BOARD_USART_PIN_CLOCK_BIT      RCC_APB2ENR_IOPAEN
+# define BOARD_PIN_TX                   GPIO9
+# define BOARD_PIN_RX                   GPIO10
+# define BOARD_USART_PIN_CLOCK_REGISTER RCC_AHBENR
+# define BOARD_USART_PIN_CLOCK_BIT      RCC_AHBENR_GPIOAEN
 
-# define BOARD_FORCE_BL_PIN             GPIO5
-# define BOARD_FORCE_BL_PORT            GPIOB
-# define BOARD_FORCE_BL_CLOCK_REGISTER  RCC_APB2ENR
-# define BOARD_FORCE_BL_CLOCK_BIT       RCC_APB2ENR_IOPBEN
-# define BOARD_FORCE_BL_PULL            GPIO_CNF_INPUT_FLOAT // depend on external pull
-# define BOARD_FORCE_BL_VALUE           BOARD_FORCE_BL_PIN
+// Force bootloader pin is not present on our board
+//# define BOARD_FORCE_BL_PIN             GPIO5
+//# define BOARD_FORCE_BL_PORT            GPIOB
+//# define BOARD_FORCE_BL_CLOCK_REGISTER  RCC_APB2ENR
+//# define BOARD_FORCE_BL_CLOCK_BIT       RCC_APB2ENR_IOPBEN
+//# define BOARD_FORCE_BL_PULL            GPIO_CNF_INPUT_FLOAT // depend on external pull
+//# define BOARD_FORCE_BL_VALUE           BOARD_FORCE_BL_PIN
 
-# define BOARD_FLASH_SECTORS            60
-# define BOARD_TYPE                     10
-# define FLASH_SECTOR_SIZE              0x400
+# define BOARD_FLASH_SECTORS            16
+# define BOARD_TYPE                     12
+# define FLASH_SECTOR_SIZE              0x1000
 
 #else
 # error Undefined Target Hardware
