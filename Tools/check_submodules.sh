1c03f0556d3a9e18ea753a8605c579cf17d05269
David Sidrane <david_s5@nscdg.com>
2018-06-29 10:40:53 -0700

    Tools:Added Git submodule checking that obeys GIT_SUBMODULES_ARE_EVIL

--- Tools/check_submodules.sh
+++ Tools/check_submodules.sh
@@ -1,27 +1,63 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [ -d libopencm3 ];
+[ -n "$GIT_SUBMODULES_ARE_EVIL" ] && {
+    # GIT_SUBMODULES_ARE_EVIL is set, meaning user doesn't want submodules
+    echo "Skipping submodules."
+    exit 0
+}
+
+GITSTATUS=$(git status)
+
+function check_git_submodule {
+
+if [ -d $1 ];
 	then
-	STATUSRETVAL=$(git submodule summary | grep -A20 -i "libopencm3" | grep "<")
+	SUBMODULE_STATUS=$(git submodule summary "$1")
+	STATUSRETVAL=$(echo $SUBMODULE_STATUS | grep -A20 -i "$1" | grep "<")
 	if [ -z "$STATUSRETVAL" ]; then
-		echo "Checked libopencm3 submodule, correct version found"
+		echo "Checked $1 submodule, correct version found"
 	else
+		echo -e "\033[31mChecked $1 submodule, ACTION REQUIRED:\033[0m"
+		echo ""
+		echo -e "New commits required:"
+		echo -e "$SUBMODULE_STATUS"
 		echo ""
 		echo ""
-		echo "   libopencm3 sub repo not at correct version. Try 'git submodule update'"
-		echo "   or follow instructions on http://px4.io/dev/git/submodules"
+		echo -e " *******************************************************************************"
+		echo -e " *   \033[31mIF YOU DID NOT CHANGE THIS FILE (OR YOU DON'T KNOW WHAT A SUBMODULE IS):\033[0m  *"
+		echo -e " *   \033[31mHit 'u' and <ENTER> to update ALL submodules and resolve this.\033[0m            *"
+		echo -e " *   (performs \033[94mgit submodule update --init --recursive\033[0m)                        *"
+		echo -e " *******************************************************************************"
 		echo ""
-		echo "   DO NOT FORGET TO RUN 'make clean' AFTER EACH libopencm3 UPDATE!"
 		echo ""
+		echo -e "   Only for EXPERTS:"
+		echo -e "   $1 submodule is not in the recommended version."
+		echo -e "   Hit 'y' and <ENTER> to continue the build with this version. Hit <ENTER> to resolve manually."
+		echo -e "   Use \033[94mgit add $1 && git commit -m 'Updated $1'\033[0m to choose this version (careful!)"
 		echo ""
-		echo "New commits required:"
-		echo "$(git submodule summary)"
-		echo ""
-		exit 1
+		read user_cmd
+		if [ "$user_cmd" == "y" ]
+		then
+			echo "Continuing build with manually overridden submodule.."
+		else
+			if [ "$user_cmd" == "u" ]
+			then
+				git submodule update --init --recursive
+				echo "Submodule fixed, continuing build.."
+			else
+				echo "Build aborted."
+				exit 1
+			fi
+		fi
 	fi
 else
-	git submodule init;
+	git submodule update --init --recursive;
 	git submodule update;
 fi
+
+}
+
+check_git_submodule libopencm3
+check_git_submodule lib/kinetis/NXP_Kinetis_Bootloader_2_0_0
 
 exit 0
