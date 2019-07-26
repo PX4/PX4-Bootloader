0543d96b079ab98689c94f401bbbf229e12cb0b1
David Sidrane <David.Sidrane@NscDg.com>
2019-07-26 03:34:27 -0700

    Added FMUv5X

--- hw_config.h
+++ hw_config.h
@@ -383,6 +383,51 @@
  * # define BOARD_FORCE_BL_CLOCK_BIT       RCC_AHB1ENR_IOPEEN
  * # define BOARD_FORCE_BL_PULL            GPIO_PUPD_PULLUP
 */
+/****************************************************************************
+ * TARGET_HW_PX4_FMU_V5X
+ ****************************************************************************/
+
+#elif  defined(TARGET_HW_PX4_FMU_V5X)
+
+# define APP_LOAD_ADDRESS               0x08008000
+# define BOOTLOADER_DELAY               5000
+# define INTERFACE_USB                  1
+# define INTERFACE_USART                1
+# define USBDEVICESTRING                "PX4 BL FMU v5X.x"
+# define USBPRODUCTID                   0x0033
+# define USBVENDORID                    0x3185
+# define USBMFGSTRING                   "Auterion"
+# define BOOT_DELAY_ADDRESS             0x000001a0
+
+# define BOARD_TYPE                     51
+# define _FLASH_KBYTES                  (*(uint16_t *)0x1ff0f442)
+# define BOARD_FLASH_SECTORS            ((_FLASH_KBYTES == 0x400) ? 7 : 11)
+# define BOARD_FLASH_SIZE               (_FLASH_KBYTES * 1024)
+
+# define OSC_FREQ                       16
+
+# define BOARD_PIN_LED_ACTIVITY         GPIO5 // BLUE
+# define BOARD_PIN_LED_BOOTLOADER       GPIO4 // GREEN
+# define BOARD_PORT_LEDS                GPIOE
+# define BOARD_CLOCK_LEDS               RCC_AHB1ENR_GPIOEEN
+# define BOARD_LED_ON                   gpio_clear
+# define BOARD_LED_OFF                  gpio_set
+
+# define BOARD_USART                    UART5
+# define BOARD_USART_CLOCK_REGISTER     RCC_APB1ENR
+# define BOARD_USART_CLOCK_BIT          RCC_APB1ENR_UART5EN
+
+# define BOARD_PORT_USART_AF_TX         GPIO_AF7
+# define BOARD_PORT_USART_TX            GPIOB
+# define BOARD_PIN_TX                   GPIO9
+# define BOARD_PORT_USART_AF_RX         GPIO_AF8
+# define BOARD_PORT_USART_RX            GPIOD
+# define BOARD_PIN_RX                   GPIO2
+# define BOARD_USART_PIN_CLOCK_REGISTER RCC_AHB1ENR
+# define BOARD_USART_PIN_CLOCK_BIT_TX   RCC_AHB1ENR_GPIOBEN
+# define BOARD_USART_PIN_CLOCK_BIT_RX   RCC_AHB1ENR_GPIODEN
+# define SERIAL_BREAK_DETECT_DISABLED   1
+
 
 /****************************************************************************
  * TARGET_HW_MINDPX_V2
