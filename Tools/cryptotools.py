28b0d992e8a3a0ebc83683159bbe71f7d8930343
dino <dino@auterion.com>
2019-08-27 10:44:31 +0200

    py script for crypto stuff

---
+++ Tools/cryptotools.py
@@ -0,0 +1,20 @@
+import nacl.encoding
+import nacl.signing
+import monocypher
+
+# Generate a new random signing key
+signing_key = nacl.signing.SigningKey.generate()
+
+# Sign a message with the signing key
+signed = signing_key.sign(b"Attack at Dawn")
+
+# Obtain the verify key for a given signing key
+verify_key = signing_key.verify_key
+
+# Serialize the verify key to send it to a third party
+verify_key_hex = verify_key.encode(encoder=nacl.encoding.HexEncoder)
+print(verify_key_hex)
+print(signed)
+print(signed)
+print(signing_key)
+
